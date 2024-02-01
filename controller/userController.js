56733598dd798e060018ae3804f48c7e133a0848
Чиноргил Амартүвшин <HP230641@G303-CC0126.local>
2024-02-01 16:31:12 +0800

    added login feature

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,14 @@
 const { userModel } = require("../database/schema/userschema")
+const bcrypt = require("bcrypt")
 
 const createUser = async (req, res) => {
-    const body = req.body
-    console.log(body)
+    const data = req.body
+    const pass = req.body.password
+    const protectedPass = bcrypt.hashSync(pass, 10)
+    const userInfo = { ...data, password: protectedPass }
+    console.log(userInfo)
     try {
-        const user = await userModel.create(body)
-        console.log(user)
+        await userModel.create(userInfo)
         res.status(201).send({ success: true })
     } catch (err) {
         console.log(err)
@@ -13,6 +16,20 @@ const createUser = async (req, res) => {
     }
 }
 
+const loginUser = async (req, res) => {
+    const body = req.body
+    const user = await userModel.findOne({ email: body.email })
+    try {
+        if (user) {
+            res.status(200).send({ success: true, user })
+        } else {
+            res.status(404).send("User not found")
+        }
+    } catch (err) {
+        res.status(500).send("Server error")
+    }
+}
 
 
-module.exports = { createUser }
+
+module.exports = { createUser, loginUser }
--- router/useRouter.js
+++ router/useRouter.js
@@ -1,7 +1,46 @@
 const Router = require("express")
-const { createUser } = require("../controller/userController")
+const { createUser, loginUser } = require("../controller/userController")
+const { userModel } = require("../database/schema/userschema")
+const bcrypt = require("bcrypt")
 
+const emailValidation = async (req, res, next) => {
+    const body = req.body;
+    const Users = await userModel.findOne({ email: body.email })
+    try {
+        if (Users) {
+            res.status(400).send({ message: "Email already in use" })
+        } else {
+            next()
+        }
+    } catch (err) {
+        res.status(500).send("Server error")
+    }
+
+}
+
+const passCheck = async (req, res, next) => {
+    const body = req.body
+    const user = await userModel.findOne({ email: body.email })
+    console.log(user)
+    try {
+        if (user) {
+            const passchecker = bcrypt.compareSync(body.password, user.password)
+            console.log(passchecker)
+            if (passchecker) {
+                next()
+            } else {
+                res.status(400).send("Password is incorrect")
+            }
+        } else {
+            res.status(404).send("Account not found")
+        }
+    } catch (err) {
+        res.status(500).send("Server error")
+    }
+
+}
 const userRouter = Router()
-userRouter.post("/users", createUser)
+userRouter.post("/users", emailValidation, createUser)
+userRouter.post("/user", passCheck, loginUser)
 
 module.exports = userRouter
