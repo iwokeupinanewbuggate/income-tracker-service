404f6e592550ca01d3282228b2ab21b4e4a906e9
Charlie <147712627+iwokeupinanewbuggate@users.noreply.github.com>
2024-03-25 22:37:11 +0800

    I forgot what i did

--- database/schema/transaction.js
+++ database/schema/transaction.js
@@ -1,21 +1,21 @@
-const { mongoose, Schema } = require("mongoose")
+const { mongoose, Schema } = require("mongoose");
 
 const transaction = new Schema({
-    userId: String,
-    category: {
-        type: String,
-        enum: ["Bill", "Food", "Clothing", "Salaries", "Donation", "Gift", "Lend"]
-    },
-    transactionTitle: String,
-    amount: Number,
-    note: String,
-    createdAt: { type: Date, default: Date.now() },
-    transactionType: {
-        type: String,
-        enum: ["income", "expense"]
-    }
-})
+  userId: String,
+  category: {
+    type: String,
+    enum: ["Bill", "Food", "Clothing", "Salaries", "Donation", "Gift", "Lend"],
+  },
+  transactionTitle: String,
+  amount: Number,
+  note: String,
+  createdAt: { type: Date, default: Date.now() },
+  transactionType: {
+    type: String,
+    enum: ["income", "expense"],
+  },
+});
 
-const transactionModel = mongoose.model("transaction", transaction)
+const transactionModel = mongoose.model("transaction", transaction);
 
-module.exports = transactionModel
+module.exports = transactionModel;
